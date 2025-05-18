f8b60667d341bf5b27557cb487e99fa4ec8426f7
localdev0 <bluerblx160@gmail.com>
2025-05-18 14:17:56 -0400

    Update jv85rps4w6hxg8dy4lc2i2511oabn7ay62.lua

--- jv85rps4w6hxg8dy4lc2i2511oabn7ay62.lua
+++ jv85rps4w6hxg8dy4lc2i2511oabn7ay62.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://gist.githubusercontent.com/localdev0/dc1fd02c9aad10d13f8250ba122a27a6/raw/c5294ed3e974f998884d14ff8f940520f2e4ed7b/gistfile1.txt"))()
+loadstring(game:HttpGet("https://gist.githubusercontent.com/localdev0/dc1fd02c9aad10d13f8250ba122a27a6/raw/2442bbbf5538867c23fe8d87ce5a5e29373fdab0/gistfile1.txt"))()
